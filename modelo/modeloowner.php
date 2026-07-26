a0b2fd320afbcb76bd8474f736243c1541e82347
gomarc2026-cmd <gomarc.2026@gmail.com>
2026-07-26 14:03:55 -0500

    hhh

--- modelo/modeloowner.php
+++ modelo/modeloowner.php
@@ -1,52 +1,52 @@
 <?php
-class Modelo {
+class Modelo{
 
-    private $owner;
-    private $db;
+  private $owner;
+  private $db;
 
-    public function __construct() {
-        $this->owner = array();
-        // Importamos la conexión PDO ya configurada con Aiven
-        require_once '../assets/db/config.php';
-        $this->db = $connect;
+  public function __construct(){
+      $this->owner=array();
+      $this->db = new PDO('mysql:host=mysql-2f8d2d20-gomarc-7589.b.aivencloud.com;port=16189;dbname=defaultdb;charset=utf8mb4', "avnadmin", "AVNS_4MBCc7IoLnFTUGHDU_R");
+  }
+  public function mostrar($tabla,$condicion){
+      $consulta="SELECT * FROM owner";
+
+      $resultado=$this->db->query($consulta);
+      while ($tabla=$resultado->fetchAll(PDO::FETCH_ASSOC)) {
+          $this->owner[]=$tabla;
+      }
+      return $this->owner;
     }
+    public function  insertar(Modelo $data){
+    try {
+    $query="INSERT INTO owner (dni_due,  nom_due )VALUES(?,?)";
 
-    public function mostrar($tabla, $condicion) {
-        $consulta = "SELECT * FROM owner";
-        $resultado = $this->db->query($consulta);
-        while ($tabla = $resultado->fetchAll(PDO::FETCH_ASSOC)) {
-            $this->owner[] = $tabla;
-        }
-        return $this->owner;
-    }
+      $this->db->prepare($query)->execute(array($data->dni_due, $data->nom_due));
 
-    public function insertar(Modelo $data) {
-        try {
-            $query = "INSERT INTO owner (dni_due, nom_due) VALUES (?, ?)";
-            $this->db->prepare($query)->execute(array($data->dni_due, $data->nom_due));
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
+    }catch (Exception $e) {
 
-    public function actualizar($tabla, $data, $condicion) {
-        $consulta = "UPDATE $tabla SET $data WHERE $condicion";
-        $resultado = $this->db->query($consulta);
-        if ($resultado) {
-            return true;
-        } else {
-            return false;
-        }
+      die($e->getMessage());
     }
-
-    public function eliminar($tabla, $condicion) {
-        $consulta = "DELETE FROM $tabla WHERE $condicion";
-        $resultado = $this->db->query($consulta);
-        if ($resultado) {
-            return true;
-        } else {
-            return false;
-        }
     }
+	
+  public function actualizar($tabla,$data,$condicion){
+      $consulta="UPDATE $tabla SET $data WHERE $condicion";
+      $resultado=$this->db->query($consulta);
+      if($resultado){
+          return true;
+      }else{
+          return false;
+      }
+  }
+  public function eliminar($tabla,$condicion){
+      $consulta="DELETE FROM $tabla WHERE $condicion";
+      $resultado=$this->db->query($consulta);
+      if($resultado){
+          return true;
+      }else{
+          return false;
+      }
+  }
 }
-?>
+
+ ?>
