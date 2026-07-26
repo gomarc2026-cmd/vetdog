19fbc25da943db9e52b046b41b3ba3eba17cfe43
gomarc2026-cmd <gomarc.2026@gmail.com>
2026-07-25 22:25:21 -0500

    subida inicial

---
+++ assets/db/config.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// Define database
+define('dbhost', 'mysql-2f8d2d20-gomarc-7580.b.aivencloud.com');
+define('dbport', '16189');
+define('dbuser', 'avnadmin');
+define('dbpass', 'AVNS_4M0Ce7IoLmFTuGHDU_R');
+define('dbname', 'defaultdb');
+
+// Connecting database
+try {
+    $options = array(
+        PDO::MYSQL_ATTR_SSL_CA => true,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    $connect = new PDO("mysql:host=".dbhost.";port=".dbport.";dbname=".dbname, dbuser, dbpass, $options);
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
